84f16dc07bec5b9968ed609a7e73cb10f7a62b9c
nyghtly-derek <derekblankenship05@gmail.com>
2018-04-24 19:22:52 -0400

    Implemented javascript code to pull data from free code camp weather api and display local temperature and conditions. Added a button for user that switches temperature display between fahrenheit and celsius.

--- get-weather.js
+++ get-weather.js
@@ -1,56 +1,87 @@
 const addressDisplay = document.getElementById("js-location");
 const tempDisplay = document.getElementById("js-temp");
 const tempTypeDisplay = document.getElementById("js-temptype");
+const conditionsDisplay = document.getElementById("js-conditions");
 
 let lat = -1;
 let long = -1;
-let tempType = "celsius";
-
-/** get weather data from fcc api **/
-
-async function getWeather() {
-    await getLocation(); 
-    const requestURL = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long;
-    httpGet(requestURL, getTemp);
-}
-
-function getTemp(weatherJson) {
-    let temp = weatherJson.main.temp;
-    if (tempType == "celsius") {
-        tempDisplay.innerHTML = temp;
-    }
-}
-
-/** find user location using google maps api **/
+let tempType = "f";
+let tempCel = 0;
+let tempFahren = 0;
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(reverseGeocode, geoError);
+        navigator.geolocation.getCurrentPosition(useLocation, geoError);
     } else {
         addressDisplay.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function reverseGeocode(position) {
+function useLocation(position) {
     lat = position.coords.latitude;
     long = position.coords.longitude;
+    reverseGeocode(lat, long);
+    const fccApiRequest = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long;
+    httpGet(fccApiRequest, getWeather);
+}
+
+function reverseGeocode(lat, long) {
     const latlng = new google.maps.LatLng(lat, long);
     const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-        if (status !== google.maps.GeocoderStatus.OK) { 
-            addressDisplay.innerHTML = "location not found";
-        }
-        else {
-            const userAddress = results[3].formatted_address;
-            console.log(userAddress);
-            addressDisplay.innerHTML = 
-                userAddress;
-        }
-    });
+    geocoder.geocode({ 'latLng': latlng }, displayLocation);
+}
+
+function displayLocation(results, status) {
+    if (status !== google.maps.GeocoderStatus.OK) { 
+        addressDisplay.innerHTML = "location not found";
+    } 
+    else {
+        const userAddress = results[3].formatted_address;
+        console.log(userAddress);
+        addressDisplay.innerHTML = userAddress;
+    }
+}
+
+function getWeather(weatherJson) {
+    tempCel = weatherJson.main.temp;
+    tempFahren = toFahrenheit(tempCel);
+    let conditions = weatherJson.weather[0].main;
+    displayTemp(tempType);
+    displayConditions(conditions);
+}
+
+function displayTemp(tempType) {
+    if (tempType == "c") {
+        tempDisplay.innerHTML = tempCel;
+        tempTypeDisplay.innerHTML = "C";
+    }
+    else if (tempType == "f") {
+        tempDisplay.innerHTML = tempFahren;
+        tempTypeDisplay.innerHTML = "F";
+    }
+}
+
+function toFahrenheit(tempC) {
+    tempF = tempCel * (9/5) + 32;
+    return tempF;
+}
+
+function displayConditions(conditions) {
+    conditionsDisplay.innerHTML = conditions;
 }
 
 function geoError() {
     addressDisplay.innerHTML = "Error: user position not found.";
+}
+
+function switchTempType() {
+    if (tempType == "c") {
+        tempType = "f";
+    }
+    else if (tempType == "f") {
+        tempType = "c";
+    }
+    displayTemp(tempType);
 }
 
 /** httpGet function borrowed from stack overflow user Joan and modified (commented for educational benefit) **/
